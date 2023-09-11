525a339f68cd3a72f3d51b68df4d8b0c82c4aaba
Stronauta <samircastillop@gmail.com>
2023-09-11 16:32:26 -0400

    Todo

---
+++ DAL/Contexto.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RegistroCliente.Models;
+
+namespace RegistroCliente.DAL
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Clientes> Clientes { get; set; }
+
+        public Contexto(DbContextOptions<Contexto> options) : base(options)
+        {
+        }
+    }
+}
---
+++ Migrations/20230910223442_Inicia.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RegistroCliente.Migrations
+{
+    /// <inheritdoc />
+    public partial class Inicia : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Clientes",
+                columns: table => new
+                {
+                    ClientesId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Nombres = table.Column<string>(type: "TEXT", nullable: false),
+                    Telefono = table.Column<int>(type: "INTEGER", nullable: false),
+                    Celular = table.Column<int>(type: "INTEGER", nullable: false),
+                    Rnc = table.Column<int>(type: "INTEGER", nullable: false),
+                    Gmail = table.Column<string>(type: "TEXT", nullable: false),
+                    Direccion = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clientes", x => x.ClientesId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Clientes");
+        }
+    }
+}
